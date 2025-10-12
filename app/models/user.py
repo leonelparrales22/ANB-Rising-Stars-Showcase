c67b624643b20861dcac68395c5aff7a4122bdd1
Leonel <leonelparrales22@gmail.com>
2025-10-11 19:29:16 -0500

    Pruebas Unitarias para el módulo Auth (#3)
    * feat(login): implement auth system with PostgreSQL and Docker
    
    * Remove INSTRUCCIONES_BORRAR.txt from repo and add INSTRUCCIONES to .gitignore
    
    * chore: update .gitignore to include INSTRUCCIONES file
    
    * fix: update secret key in .env.example and config.py for consistency
    
    * feat(auth): enhance signup and login responses with localized messages
    fix(init_db): update print statement to display message in Spanish
    
    * feat(docs): update README and add Postman collection and environment for API testing
    
    * feat(docker): update Dockerfile to include netcat installation and set entrypoint script
    feat(config): refactor config.py to use ConfigDict for environment variable management
    chore(requirements): add pydantic-settings to requirements.txt
    feat(entrypoint): create entrypoint.sh for database readiness and application startup
    
    * refactor(auth): replace Response with JSONResponse for signup response
    refactor(security): change password hashing scheme to pbkdf2_sha256
    refactor(init_db): enhance database initialization logging
    refactor(user): remove redundant Base declaration in User model
    
    * feat(docs): add sequence diagram for video upload processing flow
    
    * feat(docs): add sequence diagram for video processing and remove old flow model
    
    * feat(tests): add user signup and login tests with validation checks
    
    * feat(docs): add unit testing instructions to README and update requirements
    
    ---------
    
    Co-authored-by: Leonel Parrales M <parralesdl@produbanco.com>

--- app/models/user.py
+++ app/models/user.py
@@ -1,13 +1,14 @@
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
+import uuid
 from ..core.database import Base
 
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(String, primary_key=True, index=True)
+    id = Column(String, primary_key=True, default=str(uuid.uuid4()), index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
